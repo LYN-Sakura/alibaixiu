3960f92a185c3e1079c86bb7dd1dbd9e80ac54ef
sakura <lyn-sakura@qq.com>
2019-12-08 20:47:27 +0800

    初步增加轮播图片（未完成）

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,22 @@
 //todo
+$.when(
+	$.ajax({
+		url: "/posts/count",
+		type: "get"
+	}),
+	$.ajax({
+		url: "/categories/count",
+		type: "get"
+	}),
+	$.ajax({
+		url: "/comments/count",
+		type: "get"
+	})
 
-
+).done(function(r1, r2, r3) {
+	$("#num").html(template("numTpl", {
+		r1: r1[0],
+		r2: r2[0],
+		r3: r3[0]
+	}))
+})
---
+++ public/assets/js/slides.js
@@ -0,0 +1,30 @@
+//显示轮播图列表
+loadImg()
+function loadImg() {
+	$.ajax({
+		url: "/slides",
+		type: "get",
+		success: function(res) {
+			console.log(res)
+		}
+	})
+}
+
+
+$("#image").on("change",function(){
+	let formData = new FormData()
+	formData.append("img",this.files[0])
+	
+	$.ajax({
+		url:"/upload",
+		type:"post",
+		data:formData,
+		processData:false,
+		contentType:false,
+		success:function(res){
+			$("#img").prop("src",res[0].img)
+			$("#img").prop()
+		$("#hide").val(res[0].img)	
+		}
+	})
+})
